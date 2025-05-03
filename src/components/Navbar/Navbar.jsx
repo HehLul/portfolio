341fefd1822266894b1a804550827f0c7811d98a
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-03 12:40:07 -0400

    feat: add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Salman.</h1>
+      <ul>
+        <a href="">About me</a>
+        <a href="">My Works</a>
+        <a href="">Blog</a>
+      </ul>
+
+      <div className="buttons">
+        <button>Lets Connect</button>
+        <button>See My Work</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
