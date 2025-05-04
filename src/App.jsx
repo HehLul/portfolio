e338f73b411b1a149c1d90939e1c6468c85c743b
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-04 19:37:23 -0400

    feat: added react router to app.jsx + init projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+
 function App() {
   return (
-    <div className="app-container">
-      <Navbar></Navbar>
-      <main className="content">
-        <Home />
-      </main>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar></Navbar>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+// import "./Home.css";
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>Projects Page</h1>
+    </div>
+  );
+}
+
+export default Projects;
