4303d6abbda73b2a72c1e6fb49ea70a4aab0abca
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-07 16:50:47 -0400

    feat: projects page skeleton

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,42 @@
-// import "./Home.css";
+import "./Projects.css";
 
 function Projects() {
   return (
     <div className="projects">
-      <h1>Projects Page</h1>
+      <div className="featured">
+        <h2>Featured</h2>
+        {/* <ul className="featured-projects">
+          <div>
+            <img src="" alt="" />
+            <h3>DeepSheep: Motize your AI Chatbot</h3>
+            <p>2025</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h3>DeepSheep: Motize your AI Chatbot</h3>
+            <p>2025</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h3>DeepSheep: Motize your AI Chatbot</h3>
+            <p>2025</p>
+          </div>
+        </ul> */}
+      </div>
+      <div className="all">
+        <div className="all-header">
+          <h2>All Projects</h2>
+          <ul className="tags">
+            <li>Featured</li>
+            <li>Archived</li>
+            <li>Website</li>
+            <li>Design</li>
+            <li>Client</li>
+            <li>Fullstack</li>
+            <li>In Progress</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
