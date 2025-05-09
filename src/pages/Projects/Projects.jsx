05b70d3c08aee04dc648a913e08f8c3f30507a86
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-09 07:53:14 -0400

    feat: add event listener to project cards

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,19 +1,30 @@
 import useProjects from "../../hooks/useProjects";
 import "./Projects.css";
 
+import { Navigate } from "react-router-dom";
+
 function Projects() {
   const { projects, loading, error } = useProjects();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading projects.</p>;
 
+  const handleClickProject = (projectId) => {
+    console.log("You clicked on project:", projectId);
+    // Navigate(`/projects/${projectId}`); // if using React Router
+  };
+
   return (
     <div className="projects">
       <div className="featured">
         <h2>Featured</h2>
         <ul className="featured-projects">
-          {projects.slice(0, 3).map((project, index) => (
-            <div key={index}>
+          {projects.slice(0, 3).map((project) => (
+            <div
+              key={project.id}
+              className="project"
+              onClick={() => handleClickProject(project.id)}
+            >
               <img src={project.image_path || "/pfp.jpg"} alt="" />
               <h3>{project.title}</h3>
               <p>{project.date_built}</p>
@@ -37,8 +48,12 @@ function Projects() {
           </ul>
         </div>
         <ul className="all-projects">
-          {projects.map((project, index) => (
-            <div key={index}>
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className="project"
+              onClick={() => handleClickProject(project.id)}
+            >
               <img src={project.image_path || "/pfp.jpg"} alt="" />
               <h3>{project.title}</h3>
               <p>{project.date_built}</p>
