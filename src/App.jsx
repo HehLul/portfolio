a103c74c4bc921e7674816ae125d931f69450a3e
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-25 07:56:11 -0400

    style: mobile responsive projects page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import ProjectPage from "./pages/ProjectPage/ProjectPage";
+import Blog from "./pages/Blog/Blog";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/projects/:slug" element={<ProjectPage />} />
+            <Route path="/blog" element={<Blog />} />
           </Routes>
         </main>
         <Footer></Footer>
---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,9 @@
+function Blog() {
+  return (
+    <div className="blog">
+      <h3>Nothing here yet..</h3>
+    </div>
+  );
+}
+
+export default Blog;
